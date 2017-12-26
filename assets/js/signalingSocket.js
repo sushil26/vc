8d4a31335199960add00f8b2f717f31b1bca388b
Your Name <sushil.careator@gmail.com>
2017-12-26 14:39:39 +0530

    jghgd

--- assets/js/signalingSocket.js
+++ assets/js/signalingSocket.js
@@ -856,6 +856,7 @@ function setup_local_media(callback, errorback) {
                                 // local_media_shareStream = stream;
                                 var local_media = USE_VIDEO ? $("<video>") : $();
                                 local_media.attr("autoplay", "autoplay");
+                                local_media.attr("Volume", "false");
                                 local_media.attr("muted", "true"); /* always mute ourselves by default */
                                 local_media.attr("controls", "");
                                 local_media.attr("id", "videoElem");
