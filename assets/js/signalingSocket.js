4e63126a18bd3e34df1b541a55650b085579cb91
sushil <sushil.careator@gmail.com>
2018-01-04 11:25:00 +0530

    jhdjhdjh

--- assets/js/signalingSocket.js
+++ assets/js/signalingSocket.js
@@ -60,7 +60,7 @@ function crdcheck() {
     else {
         console.log("Password Not Matched");
         document.getElementById('videoConfStart').disabled = true;
-        window.location="https://svcapp.herokuapp.com/client";
+        window.location = "https://svcapp.herokuapp.com/client";
 
     }
 
@@ -960,3 +960,158 @@ function scrollDown() {
     $('#popupMsg').animate({ scrollTop: $('#popupMsg').prop("scrollHeight") }, 500);
     console.log("<--scrollDown");
 }
+
+// getScreenId.js
+// <script src="https://cdn.WebRTC-Experiment.com/getScreenId.js"></script>
+
+(function () {
+    window.getScreenId = function (callback) {
+        // for Firefox:
+        // sourceId == 'firefox'
+        // screen_constraints = {...}
+        if (!!navigator.mozGetUserMedia) {
+            callback(null, 'firefox', {
+                video: {
+                    mozMediaSource: 'window',
+                    mediaSource: 'window'
+                }
+            });
+            return;
+        }
+
+        window.addEventListener('message', onIFrameCallback);
+
+        function onIFrameCallback(event) {
+            if (!event.data) return;
+
+            if (event.data.chromeMediaSourceId) {
+                if (event.data.chromeMediaSourceId === 'PermissionDeniedError') {
+                    callback('permission-denied');
+                } else {
+                    callback(null, event.data.chromeMediaSourceId, getScreenConstraints(null, event.data.chromeMediaSourceId));
+                }
+
+                // this event listener is no more needed
+                window.removeEventListener('message', onIFrameCallback);
+            }
+
+            if (event.data.chromeExtensionStatus) {
+                callback(event.data.chromeExtensionStatus, null, getScreenConstraints(event.data.chromeExtensionStatus));
+
+                // this event listener is no more needed
+                window.removeEventListener('message', onIFrameCallback);
+            }
+        }
+
+        setTimeout(postGetSourceIdMessage, 100);
+    };
+
+    function getScreenConstraints(error, sourceId) {
+        var screen_constraints = {
+            audio: false,
+            video: {
+                mandatory: {
+                    chromeMediaSource: error ? 'screen' : 'desktop',
+                    maxWidth: window.screen.width > 1920 ? window.screen.width : 1920,
+                    maxHeight: window.screen.height > 1080 ? window.screen.height : 1080
+                },
+                optional: []
+            }
+        };
+
+        if (sourceId) {
+            screen_constraints.video.mandatory.chromeMediaSourceId = sourceId;
+        }
+
+        return screen_constraints;
+    }
+
+    function postGetSourceIdMessage() {
+        if (!iframe) {
+            loadIFrame(postGetSourceIdMessage);
+            return;
+        }
+
+        if (!iframe.isLoaded) {
+            setTimeout(postGetSourceIdMessage, 100);
+            return;
+        }
+
+        iframe.contentWindow.postMessage({
+            captureSourceId: true
+        }, '*');
+    }
+
+    var iframe;
+
+    // this function is used in RTCMultiConnection v3
+    window.getScreenConstraints = function (callback) {
+        loadIFrame(function () {
+            getScreenId(function (error, sourceId, screen_constraints) {
+                if (!screen_constraints) {
+                    screen_constraints = {
+                        video: true
+                    };
+                }
+
+                callback(error, screen_constraints.video);
+            });
+        });
+    };
+
+    function loadIFrame(loadCallback) {
+        if (iframe) {
+            loadCallback();
+            return;
+        }
+
+        iframe = document.createElement('iframe');
+        iframe.onload = function () {
+            iframe.isLoaded = true;
+
+            loadCallback();
+        };
+        iframe.src = 'https://www.webrtc-experiment.com/getSourceId/'; // https://wwww.yourdomain.com/getScreenId.html
+        iframe.style.display = 'none';
+        (document.body || document.documentElement).appendChild(iframe);
+    }
+
+    window.getChromeExtensionStatus = function (callback) {
+        // for Firefox:
+        if (!!navigator.mozGetUserMedia) {
+            callback('installed-enabled');
+            return;
+        }
+
+        window.addEventListener('message', onIFrameCallback);
+
+        function onIFrameCallback(event) {
+            if (!event.data) return;
+
+            if (event.data.chromeExtensionStatus) {
+                callback(event.data.chromeExtensionStatus);
+
+                // this event listener is no more needed
+                window.removeEventListener('message', onIFrameCallback);
+            }
+        }
+
+        setTimeout(postGetChromeExtensionStatusMessage, 100);
+    };
+
+    function postGetChromeExtensionStatusMessage() {
+        if (!iframe) {
+            loadIFrame(postGetChromeExtensionStatusMessage);
+            return;
+        }
+
+        if (!iframe.isLoaded) {
+            setTimeout(postGetChromeExtensionStatusMessage, 100);
+            return;
+        }
+
+        iframe.contentWindow.postMessage({
+            getChromeExtensionStatus: true
+        }, '*');
+    }
+})();
