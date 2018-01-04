8f7115d97f4bf6017e75544a0a0de4fb49422dc8
sushil <sushil.careator@gmail.com>
2018-01-04 18:15:27 +0530

    jhdjhdhtytyjh

--- js/clientmain.js
+++ js/clientmain.js
@@ -104,13 +104,7 @@ jQuery(document).ready(function( $ ) {
         $("#portfolio-wrapper").fadeTo(300, 1);
       }, 300);
     });
-  
-    // jQuery counterUp
-    $('[data-toggle="counter-up"]').counterUp({
-      delay: 10,
-      time: 1000
-    });
-  
+
   
   });
   
