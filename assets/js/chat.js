4d8421aba5eb925a390eb8b8e77e1f1bef440200
sushil <sushil.careator@gmail.com>
2018-01-09 14:50:49 +0530

    jhdjhdhtytyjh

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -6,40 +6,40 @@ var videoReader = new FileReader();
 var fileReader = new FileReader();
 
 //#####  Start Auto Link Js #####//
-    (function () {
-        var autoLink,
-            slice = [].slice;
+(function () {
+    var autoLink,
+        slice = [].slice;
 
-        autoLink = function () {
-            var callback, k, linkAttributes, option, options, pattern, v;
-            options = 1 <= arguments.length ? slice.call(arguments, 0) : [];
-            pattern = /(^|[\s\n]|<[A-Za-z]*\/?>)((?:https?|ftp):\/\/[\-A-Z0-9+\u0026\u2019@#\/%?=()~_|!:,.;]*[\-A-Z0-9+\u0026@#\/%=~()_|])/gi;
-            if (!(options.length > 0)) {
-                return this.replace(pattern, "$1<a href='$2'>$2</a>");
-            }
-            option = options[0];
-            callback = option["callback"];
-            linkAttributes = ((function () {
-                var results;
-                results = [];
-                for (k in option) {
-                    v = option[k];
-                    if (k !== 'callback') {
-                        results.push(" " + k + "='" + v + "'");
-                    }
+    autoLink = function () {
+        var callback, k, linkAttributes, option, options, pattern, v;
+        options = 1 <= arguments.length ? slice.call(arguments, 0) : [];
+        pattern = /(^|[\s\n]|<[A-Za-z]*\/?>)((?:https?|ftp):\/\/[\-A-Z0-9+\u0026\u2019@#\/%?=()~_|!:,.;]*[\-A-Z0-9+\u0026@#\/%=~()_|])/gi;
+        if (!(options.length > 0)) {
+            return this.replace(pattern, "$1<a href='$2'>$2</a>");
+        }
+        option = options[0];
+        callback = option["callback"];
+        linkAttributes = ((function () {
+            var results;
+            results = [];
+            for (k in option) {
+                v = option[k];
+                if (k !== 'callback') {
+                    results.push(" " + k + "='" + v + "'");
                 }
-                return results;
-            })()).join('');
-            return this.replace(pattern, function (match, space, url) {
-                var link;
-                link = (typeof callback === "function" ? callback(url) : void 0) || ("<a href='" + url + "'" + linkAttributes + ">" + url + "</a>");
-                return "" + space + link;
-            });
-        };
+            }
+            return results;
+        })()).join('');
+        return this.replace(pattern, function (match, space, url) {
+            var link;
+            link = (typeof callback === "function" ? callback(url) : void 0) || ("<a href='" + url + "'" + linkAttributes + ">" + url + "</a>");
+            return "" + space + link;
+        });
+    };
 
-        String.prototype['autoLink'] = autoLink;
+    String.prototype['autoLink'] = autoLink;
 
-    }).call(this);
+}).call(this);
 
 
 function autoLinkNeed() {
@@ -197,6 +197,10 @@ signaling_socket.on('newTextMsg', function (data) {
 
             document.getElementById('chatNotification').style.display = 'inline';
             document.getElementById('chatNotification').innerHTML = '(' + count + ')';
+            document.getElementById('xyz').play();
+
+
+
         }
 
 
