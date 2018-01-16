fb16ee55e96e4c9158cbb2891d83a37615568689
sushil <sushil.careator@gmail.com>
2018-01-16 12:43:39 +0530

    jhdjhdhtytyjh

--- assets/js/signalingSocket.js
+++ assets/js/signalingSocket.js
@@ -383,9 +383,9 @@ function init() {
             // vid2 = document.getElementById(peer_id + "Remote");
 
             $("#" + peer_id + "Remote").click(function () {
-console.log("sushil screen test");
+                console.log("sushil screen test");
                 $("#" + peer_id + "Remote").addClass("fullscr");
-                $("portfolio-items col-xs-12 col-sm-6 col-md-4 col-lg-3").removeClass("portfolio-items col-sm-6 col-md-4 col-lg-3");
+                $("portfolio-items col-xs-12 col-sm-6 col-md-4 col-lg-3").removeClass("portfolio-items");
 
 
             }
