e1eb720f8e116add99cfc97fb8081f70322067b4
sushil <sushil.careator@gmail.com>
2018-01-17 17:47:50 +0530

    jhdjhdhtytyjh

--- assets/js/signalingSocket.js
+++ assets/js/signalingSocket.js
@@ -348,38 +348,39 @@ function init() {
             // vid2 = document.getElementById(peer_id + "Remote");
 
             // $("#" + peer_id + "Remote").click(function () {
-                $("#" + peer_id + "fullscreenbtn2").click(function () {
+            $("#" + peer_id + "fullscreenbtn2").click(function () {
                 console.log("sushil screen test");
                 $("#" + peer_id + "remoteVideoElement").addClass("fullscr");
                 $("#" + peer_id + "remoteContainer").removeClass("portfolio-items col-xs-12 col-sm-6 col-md-4 col-lg-3");
-                $("#" + peer_id + "Remote").css({"height":"100vh"});
+                $("#" + peer_id + "Remote").css({ "height": "100vh" });
 
-                $("#videoElem").css({ "position": "absolute","top": "5%","left": "5%","z-index": "2","background": "none","border": "none","height": "auto","width": "20%"});
+                $("#videoElem").css({ "position": "absolute", "top": "5%", "left": "5%", "z-index": "2", "background": "none", "border": "none", "height": "auto", "width": "20%" });
                 $("#videoElem111").removeClass("portfolio-items col-xs-12 col-sm-6 col-md-4 col-lg-3");
                 document.getElementById('header').style.display = 'none';
                 document.getElementById('btnrestore').style.display = 'inline';
-                document.getElementsByClassName('details').style.display='none';
-
-                $("#btnrestore").click(function(){
-                    $("#" + peer_id + "remoteVideoElement").removeClass("fullscr");
-                    $("#" + peer_id + "remoteContainer").addClass("portfolio-items col-xs-12 col-sm-6 col-md-4 col-lg-3");
-                    $("#" + peer_id + "Remote").css({"height":"200px"});
-    
-                    $("#videoElem").css({ "position": "","top": "","left": "","z-index": "","background": "","border": "","height": "","width": ""});
-                    $("#videoElem111").addClass("portfolio-items col-xs-12 col-sm-6 col-md-4 col-lg-3");
-                    document.getElementById('header').style.display = 'inline';
-                    document.getElementById('btnrestore').style.display = 'none';
-                    document.getElementsByClassName('details').style.display='inline';
-
-                })
-
-
-
-              
-        
-
+                document.getElementsByClassName('details').style.display = 'none';
             }
             )
+
+            $("#btnrestore").click(function () {
+                $("#" + peer_id + "remoteVideoElement").removeClass("fullscr");
+                $("#" + peer_id + "remoteContainer").addClass("portfolio-items col-xs-12 col-sm-6 col-md-4 col-lg-3");
+                $("#" + peer_id + "Remote").css({ "height": "200px" });
+
+                $("#videoElem").css({ "position": "", "top": "", "left": "", "z-index": "", "background": "", "border": "", "height": "", "width": "" });
+                $("#videoElem111").addClass("portfolio-items col-xs-12 col-sm-6 col-md-4 col-lg-3");
+                document.getElementById('header').style.display = 'inline';
+                document.getElementById('btnrestore').style.display = 'none';
+                document.getElementsByClassName('details').style.display = 'inline';
+
+            })
+
+
+
+
+
+
+
             // $("#fullscreenbtn2").click(function () {
             //     console.log("sushil screen test");
             //     $("#" + peer_id + "Remote").addClass("fullscr");
