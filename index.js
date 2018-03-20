a886e0666e013395fdccb40809f37d3a5b21ae77
shaiksajans <shaiksajans@gmail.com>
2018-03-20 13:24:26 +0530

    sc

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use(express.static(__dirname + '/public'));
 // app.use(express.static(__dirname + '/node_modules'));
 app.get('/', function (req, res) {
 
-    res.sendFile(__dirname + './index.html');
+    res.sendFile(__dirname + 'public/index.html');
     
 });
 
