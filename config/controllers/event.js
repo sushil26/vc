4fe332c43adc929e092691d638b44230b05e5dfa
LogeswariGuru <logeswari.careator@gmail.com>
2018-03-26 12:34:16 +0530

    conadd

--- config/controllers/event.js
+++ config/controllers/event.js
@@ -173,6 +173,7 @@ module.exports.parentCredential = function (req, res) {
         
         event.find({'url':req.body.url}).toArray(function (err, data) {
             if (data.length > 0) {
+                console.log("data[0].password: "+data[0].password);
                 if (data[0].password == req.body.pswd) {
                     console.log("Successfully Logged in");
                     responseData = {
