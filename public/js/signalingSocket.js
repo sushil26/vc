0a67a1e523f57bea83ebb30106306953046c81fa
LogeswariGuru <logeswari.careator@gmail.com>
2018-04-02 11:43:19 +0530

    echo

--- public/js/signalingSocket.js
+++ public/js/signalingSocket.js
@@ -951,8 +951,9 @@ function setup_local_media(callback, errorback) {
             local_media_stream = stream;
             // local_media_shareStream = stream;
             var local_media = USE_VIDEO ? $("<video>") : $();
-            local_media.attr("autoplay", "true");
-            local_media.attr("muted", "true"); /* always mute ourselves by default */
+            // local_media.attr("autoplay", "true");
+            local_media.attr("volume", 0);
+            local_media.attr("muted", "false"); /* always mute ourselves by default */
             local_media.attr("id", "videoElem");
             local_media.attr("volume", "0.9");
 
