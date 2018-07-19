31d49b49a6187f5ff0fd639fd3aa62d401bbe39b
shaiksajans <shaiksajans@gmail.com>
2018-07-19 20:53:14 +0530

    img replaced change

--- public/js/csigsocket.js
+++ public/js/csigsocket.js
@@ -1746,24 +1746,24 @@ signaling_socket.on('comm_logoutNotifyToUserById', function (data) {
 
 })
 
-window.onbeforeunload = function (event) {
+// window.onbeforeunload = function (event) {
 
-  if (stuff.length > 5) {
-    var message = 'Sure you want to leave?';
-    if (typeof event == 'undefined') {
-      event = window.event;
-    }
-    if (event) {
-      event.returnValue = message;
-    }
-    console.log("message: " + message);
-    return message;
-  }
-  else {
-    this.alert("super");
-  }
+//   if (stuff.length > 5) {
+//     var message = 'Sure you want to leave?';
+//     if (typeof event == 'undefined') {
+//       event = window.event;
+//     }
+//     if (event) {
+//       event.returnValue = message;
+//     }
+//     console.log("message: " + message);
+//     return message;
+//   }
+//   else {
+//     this.alert("super");
+//   }
 
-}
+// }
 
 
 
